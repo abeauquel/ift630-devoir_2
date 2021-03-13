35bd33754d2ce06d0653c9cfb8d89570aa5337b3
abeauquel <alexandre.beauquel@celiersintelligents.com>
2021-03-13 16:48:43 -0500

    convert int to double

--- question1/laplaceJacobi.cpp
+++ question1/laplaceJacobi.cpp
@@ -2,30 +2,30 @@
 #include <stdio.h>
 #include "mpi.h"
 #define N               4        /* number of rows and columns in matrix */
-#define K               20      
+#define K               100      
 
 MPI_Status status;
 
-int m1[N][N]= {  
-        {1,1,1,1},  
-        {2,2,2,2},  
-        {3,3,3,3},  
-        {4,4,4,4}  
+double m1[N][N]= {  
+        {1.0,1.0,1.0,1.0},  
+        {2.0,2.0,2.0,2.0},  
+        {3.0,3.0,3.0,3.0},  
+        {4.0,4.0,4.0,4.0}  
     };
 
-int result[N][N]{  
-        {0,0,0,0},  
-        {0,0,0,0},  
-        {0,0,0,0},  
-        {0,0,0,0}  
+double result[N][N]{  
+        {0.0,0.0,0.0,0.0},  
+        {0.0,0.0,0.0,0.0},  
+        {0.0,0.0,0.0,0.0},  
+        {0.0,0.0,0.0,0.0}  
     };
 
 int main(int argc, char **argv)
 {
     int numtasks,taskid,numworkers,source,dest = 0;
     
-    int x, y, z, w, i, j, moyenne, nbProduit, nbProduitTotal = 0;
-    
+    int i, j, nbProduit, nbProduitTotal = 0;
+    double x, y, z, w, moyenne = 0.0;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
@@ -65,22 +65,22 @@ int main(int argc, char **argv)
             {
                 for (int j = 0; j < N; j++)
                 {
-                    x= (i-1) < 0 ? -1 : m1[i-1][j];
-                    y=(j-1) < 0 ? -1 : m1[i][j-1];
-                    z=(i+1) >= N ? -1 : m1[i+1][j];
-                    w=(j+1) >= N ? -1 : m1[i][j+1];
+                    x= (i-1) < 0 ? -1.0 : m1[i-1][j];
+                    y=(j-1) < 0 ? -1.0 : m1[i][j-1];
+                    z=(i+1) >= N ? -1.0 : m1[i+1][j];
+                    w=(j+1) >= N ? -1.0 : m1[i][j+1];
 
-                    MPI_Send(&x, 1, MPI_INT, (dest % (numworkers))+2, 0, MPI_COMM_WORLD);
-                    MPI_Send(&y, 1, MPI_INT, (dest % (numworkers))+2, 0, MPI_COMM_WORLD);
-                    MPI_Send(&z, 1, MPI_INT, (dest % (numworkers))+2, 0, MPI_COMM_WORLD);
-                    MPI_Send(&w, 1, MPI_INT, (dest % (numworkers))+2, 0, MPI_COMM_WORLD);
+                    MPI_Send(&x, 1, MPI_DOUBLE, (dest % (numworkers))+2, 0, MPI_COMM_WORLD);
+                    MPI_Send(&y, 1, MPI_DOUBLE, (dest % (numworkers))+2, 0, MPI_COMM_WORLD);
+                    MPI_Send(&z, 1, MPI_DOUBLE, (dest % (numworkers))+2, 0, MPI_COMM_WORLD);
+                    MPI_Send(&w, 1, MPI_DOUBLE, (dest % (numworkers))+2, 0, MPI_COMM_WORLD);
                     MPI_Send(&i, 1, MPI_INT, (dest % (numworkers))+2, 0, MPI_COMM_WORLD);
                     MPI_Send(&j, 1, MPI_INT, (dest % (numworkers))+2, 0, MPI_COMM_WORLD);
                     dest+=1;
                 }
             }
 
-            MPI_Recv(&result, N*N, MPI_INT, 1, 0, MPI_COMM_WORLD,  &status);
+            MPI_Recv(&result, N*N, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD,  &status);
             for (size_t i = 0; i < N; i++)
             {
                 for (size_t j = 0; j < N; j++)
@@ -93,7 +93,7 @@ int main(int argc, char **argv)
         printf("Here is the result matrix:\n");
         for (i=0; i<N; i++) {
             for (j=0; j<N; j++)
-                printf("%d   ", result[i][j]);
+                printf("%f   ", result[i][j]);
             printf ("\n");
         }
     }
@@ -108,14 +108,14 @@ int main(int argc, char **argv)
             for (size_t v = 0; v < N*N; v++)
             {
 
-                MPI_Recv(&moyenne, 1, MPI_INT, (source % (numworkers))+2, 0, MPI_COMM_WORLD, &status);
+                MPI_Recv(&moyenne, 1, MPI_DOUBLE, (source % (numworkers))+2, 0, MPI_COMM_WORLD, &status);
                 MPI_Recv(&i, 1, MPI_INT, (source % (numworkers))+2, 0, MPI_COMM_WORLD, &status);
                 MPI_Recv(&j, 1, MPI_INT, (source % (numworkers))+2, 0, MPI_COMM_WORLD, &status);
                 result[i][j]=moyenne;
                 source+=1;
             }
 
-            MPI_Send(&result, N*N, MPI_INT, dest, 0, MPI_COMM_WORLD);
+            MPI_Send(&result, N*N, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD);
 
         }
         
@@ -133,15 +133,15 @@ int main(int argc, char **argv)
         for (size_t k = 0; k < nbProduit; k++)
         {
             
-            MPI_Recv(&x, 1, MPI_INT, source, 0, MPI_COMM_WORLD, &status);
-            MPI_Recv(&y, 1, MPI_INT, source, 0, MPI_COMM_WORLD, &status);
-            MPI_Recv(&z, 1, MPI_INT, source, 0, MPI_COMM_WORLD, &status);
-            MPI_Recv(&w, 1, MPI_INT, source, 0, MPI_COMM_WORLD, &status);
+            MPI_Recv(&x, 1, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, &status);
+            MPI_Recv(&y, 1, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, &status);
+            MPI_Recv(&z, 1, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, &status);
+            MPI_Recv(&w, 1, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, &status);
             MPI_Recv(&i, 1, MPI_INT, source, 0, MPI_COMM_WORLD, &status);
             MPI_Recv(&j, 1, MPI_INT, source, 0, MPI_COMM_WORLD, &status);
 
             moyenne = (x+y+z+w)/4;
-            MPI_Send(&moyenne, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
+            MPI_Send(&moyenne, 1, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD);
             MPI_Send(&i, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
             MPI_Send(&j, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
 
