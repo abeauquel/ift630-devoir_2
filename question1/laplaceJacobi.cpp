db8190d4e6f0076123078a9d2b475b771b4aa629
abeauquel <alexandre.beauquel@celiersintelligents.com>
2021-03-14 14:52:09 -0400

    nouvelle solution jacobi

--- question1/laplaceJacobi.cpp
+++ question1/laplaceJacobi.cpp
@@ -1,8 +1,11 @@
 #include <sys/time.h>
 #include <stdio.h>
 #include "mpi.h"
+#include <cmath>
 #define N               4        /* number of rows and columns in matrix */
-#define K               100      
+#define K               12  
+#define LIMIT           -1.0 
+#define EPSILON         0.1
 
 MPI_Status status;
 
@@ -22,7 +25,7 @@ double result[N][N]{
 
 int main(int argc, char **argv)
 {
-    int numtasks,taskid,numworkers,source,dest = 0;
+    int numtasks,taskid,numworkers, closeEnough, qtyToRecv, source,dest = 0;
     
     int i, j, nbProduit, nbProduitTotal = 0;
     double x, y, z, w, moyenne = 0.0;
@@ -43,53 +46,93 @@ int main(int argc, char **argv)
         printf("NumTasks : %d   \n", numtasks);
         printf("Numworkers : %d   \n", numworkers);
 
-        nbProduitTotal=N*N*K;
+        nbProduitTotal=N*N;
 
         int resteProduit = nbProduitTotal % numworkers;
 
-        for (dest=2; dest<=(numworkers+1); dest++)
-        {
-            nbProduit=nbProduitTotal/numworkers;
-            if(resteProduit > 0){
-                nbProduit+=1;
-                resteProduit-=1;
-            }
-            printf("Envoi : %d vers le worker %d  \n", nbProduit, dest);
-            MPI_Send(&nbProduit, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
-        }
-
-        for (size_t k = 0; k < K; k++)
+        for (int k = 0; k < K; k++)
         {
             dest=0;
-            for (int i = 0; i < N; i++)
-            {
-                for (int j = 0; j < N; j++)
+
+            if(closeEnough == 0){ //If there is at least another pass to do
+                for (dest=2; dest<=(numworkers+1); dest++)
                 {
-                    x= (i-1) < 0 ? -1.0 : m1[i-1][j];
-                    y=(j-1) < 0 ? -1.0 : m1[i][j-1];
-                    z=(i+1) >= N ? -1.0 : m1[i+1][j];
-                    w=(j+1) >= N ? -1.0 : m1[i][j+1];
-
-                    MPI_Send(&x, 1, MPI_DOUBLE, (dest % (numworkers))+2, 0, MPI_COMM_WORLD);
-                    MPI_Send(&y, 1, MPI_DOUBLE, (dest % (numworkers))+2, 0, MPI_COMM_WORLD);
-                    MPI_Send(&z, 1, MPI_DOUBLE, (dest % (numworkers))+2, 0, MPI_COMM_WORLD);
-                    MPI_Send(&w, 1, MPI_DOUBLE, (dest % (numworkers))+2, 0, MPI_COMM_WORLD);
-                    MPI_Send(&i, 1, MPI_INT, (dest % (numworkers))+2, 0, MPI_COMM_WORLD);
-                    MPI_Send(&j, 1, MPI_INT, (dest % (numworkers))+2, 0, MPI_COMM_WORLD);
-                    dest+=1;
+                    nbProduit=nbProduitTotal/numworkers;
+                    if(resteProduit > 0){
+                        nbProduit+=1;
+                        resteProduit-=1;
+                    }
+                    //printf("Envoi : %d vers le worker %d  \n", nbProduit,dest);
+                    MPI_Send(&nbProduit, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
                 }
-            }
 
-            MPI_Recv(&result, N*N, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD,  &status);
-            for (size_t i = 0; i < N; i++)
-            {
-                for (size_t j = 0; j < N; j++)
+                //Send the receiver how many he will receive
+                //printf("Envoi : %d vers le worker 1  \n", nbProduitTotal);
+                MPI_Send(&nbProduitTotal, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+
+
+
+                for (int i = 0; i < N; i++)
                 {
-                    m1[i][j]=result[i][j];
-                }
-            }
+                    for (int j = 0; j < N; j++)
+                    {
+                        x = (i-1) < 0 ? LIMIT : m1[i-1][j];
+                        y = (j-1) < 0 ? LIMIT : m1[i][j-1];
+                        z = (i+1) >= N ? LIMIT : m1[i+1][j];
+                        w = (j+1) >= N ? LIMIT : m1[i][j+1];
 
+                        MPI_Send(&x, 1, MPI_DOUBLE, (dest % (numworkers))+2, 0, MPI_COMM_WORLD);
+                        MPI_Send(&y, 1, MPI_DOUBLE, (dest % (numworkers))+2, 0, MPI_COMM_WORLD);
+                        MPI_Send(&z, 1, MPI_DOUBLE, (dest % (numworkers))+2, 0, MPI_COMM_WORLD);
+                        MPI_Send(&w, 1, MPI_DOUBLE, (dest % (numworkers))+2, 0, MPI_COMM_WORLD);
+                        MPI_Send(&i, 1, MPI_INT, (dest % (numworkers))+2, 0, MPI_COMM_WORLD);
+                        MPI_Send(&j, 1, MPI_INT, (dest % (numworkers))+2, 0, MPI_COMM_WORLD);
+                        dest+=1;
+                    }
+                }
+
+                MPI_Recv(&result, nbProduitTotal, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD,  &status);
+                closeEnough = 1;
+                for (size_t i = 0; i < N; i++)
+                {
+                    for (size_t j = 0; j < N; j++)
+                    {
+                        m1[i][j]=result[i][j];
+
+                        //If at least one value is not close enough, continue
+                        if(!(m1[i][j] <= LIMIT + EPSILON && m1[i][j] >= LIMIT - EPSILON)){
+                            closeEnough = 0;
+                        }
+
+                    }
+                }
+
+            }else{ //Send stop notice to all and finalize because we are close enough to the limit
+                nbProduit = 0;
+                printf("Close enough after %d iterations, stopping...\n", k);
+
+                for (dest=2; dest<=(numworkers+1); dest++)
+                {
+                    MPI_Send(&nbProduit, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
+                }
+
+                MPI_Send(&nbProduit, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+                k = K;
+            }
         }
+        if(closeEnough == 0){ //If max K reached, send stop notice
+            nbProduit = 0;
+            printf("MAX iterations reached, stopping...\n");
+
+            for (dest=2; dest<=(numworkers+1); dest++)
+            {
+                MPI_Send(&nbProduit, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
+            }
+
+            MPI_Send(&nbProduit, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        }
+
+    
         printf("Here is the result matrix:\n");
         for (i=0; i<N; i++) {
             for (j=0; j<N; j++)
@@ -100,25 +143,31 @@ int main(int argc, char **argv)
 
     /*----------- taches sur le master Receiver :------*/
     if (taskid == 1) {
-        dest, i,j=0;
-        
-        for (size_t k = 0; k < K; k++)
-        {
-            source=0;
-            for (size_t v = 0; v < N*N; v++)
-            {
+        dest, qtyToRecv, i,j=0;
 
-                MPI_Recv(&moyenne, 1, MPI_DOUBLE, (source % (numworkers))+2, 0, MPI_COMM_WORLD, &status);
-                MPI_Recv(&i, 1, MPI_INT, (source % (numworkers))+2, 0, MPI_COMM_WORLD, &status);
-                MPI_Recv(&j, 1, MPI_INT, (source % (numworkers))+2, 0, MPI_COMM_WORLD, &status);
-                result[i][j]=moyenne;
-                source+=1;
+        while(closeEnough == 0){
+            //Receive qty to receive
+            MPI_Recv(&qtyToRecv, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+            //printf("worker %d, receive nb produit %d  \n ", taskid,qtyToRecv);
+
+
+            if(qtyToRecv > 0){
+                source=0;
+                for (size_t v = 0; v < qtyToRecv; v++)
+                {
+
+                    MPI_Recv(&moyenne, 1, MPI_DOUBLE, (source % (numworkers))+2, 0, MPI_COMM_WORLD, &status);
+                    MPI_Recv(&i, 1, MPI_INT, (source % (numworkers))+2, 0, MPI_COMM_WORLD, &status);
+                    MPI_Recv(&j, 1, MPI_INT, (source % (numworkers))+2, 0, MPI_COMM_WORLD, &status);
+                    result[i][j]=moyenne;
+                    source+=1;
+                }
+
+                MPI_Send(&result, N*N, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD);
+            }else{
+                closeEnough = 1;
             }
-
-            MPI_Send(&result, N*N, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD);
-
         }
-        
     }
 
     /*---------------------------- worker----------------------------*/
@@ -127,25 +176,35 @@ int main(int argc, char **argv)
         dest = 1;
 
         MPI_Recv(&nbProduit, 1, MPI_INT, source, 0, MPI_COMM_WORLD, &status);
+        //printf("worker %d, receive nb produit %d  \n ", taskid,nbProduit);
 
-        printf("worker %d, receive nb produit %d  \n ", taskid,nbProduit);
+        if(nbProduit == 0)
+            closeEnough = 1;
 
-        for (size_t k = 0; k < nbProduit; k++)
-        {
-            
-            MPI_Recv(&x, 1, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, &status);
-            MPI_Recv(&y, 1, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, &status);
-            MPI_Recv(&z, 1, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, &status);
-            MPI_Recv(&w, 1, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, &status);
-            MPI_Recv(&i, 1, MPI_INT, source, 0, MPI_COMM_WORLD, &status);
-            MPI_Recv(&j, 1, MPI_INT, source, 0, MPI_COMM_WORLD, &status);
+        while(closeEnough == 0){
 
-            moyenne = (x+y+z+w)/4;
-            MPI_Send(&moyenne, 1, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD);
-            MPI_Send(&i, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
-            MPI_Send(&j, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
+            for (size_t k = 0; k < nbProduit; k++)
+            {
+                
+                MPI_Recv(&x, 1, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, &status);
+                MPI_Recv(&y, 1, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, &status);
+                MPI_Recv(&z, 1, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, &status);
+                MPI_Recv(&w, 1, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, &status);
+                MPI_Recv(&i, 1, MPI_INT, source, 0, MPI_COMM_WORLD, &status);
+                MPI_Recv(&j, 1, MPI_INT, source, 0, MPI_COMM_WORLD, &status);
 
+                moyenne = (x+y+z+w)/4;
+                MPI_Send(&moyenne, 1, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD);
+                MPI_Send(&i, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
+                MPI_Send(&j, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
+            }
+            MPI_Recv(&nbProduit, 1, MPI_INT, source, 0, MPI_COMM_WORLD, &status);
+            //printf("worker %d, receive nb produit %d  \n ", taskid,nbProduit);
+
+            if(nbProduit == 0)
+                closeEnough = 1;
         }
+        
     }
     
     printf("Finalize worker %d   \n", taskid);
