3ef6400d4fa5aea84c6abbe5554c65257b8aadd5
abeauquel <alexandre.beauquel@celiersintelligents.com>
2021-03-14 15:58:09 -0400

    ajout du timer en secondes

--- question2/SequencielDecodage.cpp
+++ question2/SequencielDecodage.cpp
@@ -1,7 +1,8 @@
 #include <cstdlib>
 #include <iostream>
 #include "encodage.cpp"
-using namespace std;
+#include <chrono> 
+using namespace std::chrono; 
 #define TAILLE_MOT               7  
 
 char alphabet[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
@@ -9,8 +10,9 @@ int myIndex[TAILLE_MOT] = {0,0,0,0,0,0,0};
 
 int main(int argc, char **argv)
 {   
+    auto start = high_resolution_clock::now(); 
     void incrementIndex(int rangIndex);
-    string motATrouver = "bonjour";
+    string motATrouver = "aanjour";
     string hashATrouver = encode(motATrouver);
     string motTest;
     string hashTest;
@@ -26,16 +28,22 @@ int main(int argc, char **argv)
              motTest+=alphabet[myIndex[i]];
         }
         
-        cout << motTest << "\n";
+       // cout << motTest << "\n";
         hashTest=encode(motTest);
         incrementIndex(TAILLE_MOT-1);
     }
 
-    cout << hex << "---------------------------------------- \n";
-    cout << hex << "hash trouvé : \n";
+    auto stop = high_resolution_clock::now(); 
+
+    cout << "---------------------------------------- \n";
+    cout << "mot trouvé : "<< motTest << "\n";
+    cout << "hash trouvé : \n";
     for (int i=0; i<hashTest.length(); i++)
         cout << hex << (int)hashTest[i] << " ";
     cout << "\n" << endl;
+    auto duration = duration_cast<seconds>(stop - start); 
+
+    cout << "Time taken by function: " << duration.count() << " seconds" << endl;
 }
 
 void incrementIndex(int rangIndex){
